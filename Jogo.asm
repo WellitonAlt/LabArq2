7de78001969b95306111f50814d7261e7f90f7a2
Welliton Altimari <etoalt@hotmail.com>
2017-02-05 13:51:36 -0200

    Jogo 100% funcionando

--- Jogo.asm
+++ Jogo.asm
@@ -2,14 +2,29 @@ INCLUDE Irvine32.inc
 
 .data
  outHandle    DWORD ? 
- scrSize COORD <120,50> 
+ scrSize COORD <85,50> 
 
  Ctrl BYTE 0
+ Ctrl2 BYTE 0
  Tempo DWORD ?
+ Pont WORD 0
+ Atraso DWORD 500
 
- Fila0 BYTE 0, 0, 0, 0, 0, 0, 0, 0
- Fila1 BYTE 1, 0, 0, 0, 0, 0, 0, 0
- Fila2 BYTE 0, 0, 0, 0, 0, 0, 0, 0
+ Pontuacao BYTE "Potuacao: ", 0
+
+ Opcoes BYTE "				  1 - COMECAR",0ah, 0dh
+        BYTE "				ESC - SAIR" ,0ah, 0dh
+		BYTE "				" ,0ah, 0dh
+		BYTE "				" ,0ah, 0dh
+		BYTE "				" ,0ah, 0dh
+		BYTE "				" ,0ah, 0dh
+		BYTE "	AJUDA:	Use as SETAS par CIMA e para BAIXO para desviar dos Baloes", 0ah, 0dh
+		BYTE "		para marcar o maior numero de pontos posivel, Boa Sorte :)",0
+    		
+
+ Fila0 BYTE 0, 2, 0, 0, 0, 2, 0, 0
+ Fila1 BYTE 1, 0, 0, 2, 0, 0, 0, 0
+ Fila2 BYTE 0, 2, 0, 0, 0, 2, 0, 0
 
  ZepX BYTE 4
  ZepY BYTE 4
@@ -25,7 +40,135 @@ INCLUDE Irvine32.inc
 			254, 254,  32, 254,  32, 254,  32, 254, 32,
 			 32, 254, 254, 254, 254, 254, 254, 254, 32,
 			 32,  32,  32,  32,  32, 254,  32, 254, 32
+ 
+ GameOver BYTE "		 _____                          ____                  ",0ah, 0dh                
+		  BYTE "		/ ____|                        / __ \                 ",0ah, 0dh
+		  BYTE "		| |  __  __ _ _ __ ___   ___  | |  | |_   _____ _ __  ",0ah, 0dh
+	      BYTE "		| | |_ |/ _` | '_ ` _ \ / _ \ | |  | \ \ / / _ \ '__| ",0ah, 0dh
+		  BYTE "		| |__| | (_| | | | | | |  __/ | |__| |\ V /  __/ |    ",0ah, 0dh
+		  BYTE "		\______|\__,_|_| |_| |_|\___|  \____/  \_/ \___|_|    ",0
+ 
+ Avion BYTE "			   	             _              ",0ah, 0dh
+       BYTE "			       /\           (_)			    ",0ah, 0dh
+       BYTE "			      /  \__   ___  ___  _ __	",0ah, 0dh
+       BYTE "			     / /\ \ \ / / |/ _ \| '_ \	",0ah, 0dh
+       BYTE "			    / ____ \ V /| | (_) | | | |	",0ah, 0dh
+       BYTE "		           /_/    \_\_/ |_|\___/|_| |_|	",0
+
  .code
+
+HUD PROC
+
+	mov  eax, white
+	call SetTextColor
+
+	mov dl, 1
+	mov dh, 1
+	call GotoXY
+
+	mov ecx, 80
+	mov al, 220
+
+	L1:
+	  call WriteChar
+	  inc dl
+	  call GotoXY
+	Loop L1
+
+	mov dl, 2
+	mov dh, 21
+	call GotoXY
+
+	mov ecx, 78
+	L2:
+	  call WriteChar
+	  inc dl
+	  call GotoXY
+	Loop L2
+
+	mov dl, 2
+	mov dh, 24
+	call GotoXY
+
+	mov ecx, 78
+	L3:
+	  call WriteChar
+	  inc dl
+	  call GotoXY
+	Loop L3
+
+	mov dl, 1
+	mov dh, 2
+	call GotoXY
+
+	mov ecx, 23
+	mov al, 219
+
+	L4:
+	  call WriteChar
+	  add dl, 79
+	  call GotoXY
+	  call WriteChar
+	  inc dh
+	  sub dl, 79
+	  call GotoXY
+	Loop L4
+
+
+	ret
+HUD ENDP
+
+Atualiza_Pont PROC
+	.IF Ctrl == 0
+		add Pont, 5
+		mov dl, 60
+		mov dh, 23
+		call GotoXY
+		push eax
+			mov  eax, white
+			call SetTextColor
+			movzx eax, Pont
+			call WriteDec
+		pop eax
+	.ENDIF
+	
+	ret
+Atualiza_Pont ENDP
+
+Pontu PROC
+	mov  eax, white
+	call SetTextColor
+	mov dl, 50
+	mov dh, 23
+	call GotoXY
+	mov edx, OFFSET Pontuacao
+    call WriteString 
+
+	ret
+Pontu ENDP
+
+
+Atraso_Ctrl PROC
+		movzx eax, Pont
+		.IF eax > 50 && Ctrl2 == 0
+			sub Atraso, 50
+			inc Ctrl2
+		.ELSEIF eax > 150 && Ctrl2 == 1
+			sub Atraso, 50
+			inc Ctrl2
+		.ELSEIF eax > 300 && Ctrl2 == 2
+			sub Atraso, 50
+			inc Ctrl2
+		.ELSEIF eax > 450 && Ctrl2 == 3
+			sub Atraso, 50
+			inc Ctrl2
+		.ELSEIF eax > 600 && Ctrl2 == 4
+			sub Atraso, 50
+			inc Ctrl2
+		.ENDIF
+	ret
+Atraso_Ctrl ENDP
+
 
 Gera_Zep PROC
 	call Randomize
@@ -56,8 +199,7 @@ Gera_Zep PROC
 		mov Fila2[7], 0
 		mov Ctrl, 0
 	.ENDIF
-	
-		
+			
 	ret		
 Gera_Zep ENDP
 
@@ -78,12 +220,13 @@ Move_Aviao PROC
 	.ELSE
 		call Colisao
 	.ENDIF
-
+	
 	ret
 Move_Aviao ENDP
 
 Move_ZEP PROC    
 	
+	call Atualiza_Pont
 	mov ecx, 8
 	mov ebx, 0	
 L0:	
@@ -101,7 +244,7 @@ LOOP L0
 L1:	
 	mov al, Fila1[ebx + 1]
     .IF Fila1[ebx] == 1 && al == 2
-	     call Colisao
+	    call Colisao
 	.ELSEIF Fila1[ebx] != 1
 		mov Fila1[ebx], al	
 	.ENDIF
@@ -298,6 +441,25 @@ LOOP Linha
 Apaga_Aviao ENDP
 
 Colisao PROC
+	call Clrscr
+	mov dl, 1
+	mov dh, 7
+	call GotoXY
+	mov  eax, red
+	call SetTextColor
+	mov edx, OFFSET GameOver
+    call WriteString 
+	call HUD
+	call Pontu
+	mov dl, 60 ;Transformar função
+	mov dh, 23
+	call GotoXY
+	mov  eax, white
+	call SetTextColor
+	movzx eax, Pont
+	call WriteDec
+	mov eax, 2000
+	call delay
 	exit
 Colisao ENDP
 
@@ -308,13 +470,70 @@ main PROC
 	INVOKE SetConsoleScreenBufferSize, 
 	outHandle,scrSize
 	
+	mov dl, 1
+	mov dh, 8
+	call GotoXY
+	mov  eax, white
+	call SetTextColor
+	mov edx, OFFSET Avion
+    call WriteString
+	mov dl, 1
+	mov dh, 16
+	call GotoXY
+	mov edx, OFFSET Opcoes
+    call WriteString	 
+	call HUD
+	
+	mov ecx, 0
+
+Op:	
+    mov  eax,50          
+    call Delay        
+	call ReadKey
+	push edx 
+
+	.IF al == 31h
+		call Clrscr
+		 mov AviaoX, 4
+         mov AviaoY, 10
+		jmp Continua
+	.ELSEIF ah == 50
+	    ;call ajuda	          	
+	.ENDIF
+
+	.IF ecx == 0 
+		call Apaga_Aviao
+		mov AviaoX, 3
+		mov AviaoY, 3
+	.ELSEIF ecx == 67
+		mov ecx, -1
+	.ENDIF
+	 
+	push ecx
+		call Apaga_Aviao
+		inc AviaoX
+		call Desenha_Aviao
+		mov eax, 80
+		call Delay
+	pop ecx
+	inc ecx
+
+	pop edx
+    cmp dx,VK_ESCAPE
+ jne OP
+ je Sair
+ Continua:
+
+	call HUD	
+	call Pontu
 	call Gera_Zep
 	call Escreve_Fila
 
 	call GetMseconds
 	mov Tempo,eax
+	
 
-Setas:
+Setas:	
     mov  eax,50          
     call Delay        
 	call ReadKey        
@@ -331,9 +550,10 @@ Setas:
 		call Desenha_Aviao	          	
 	.ENDIF
 
+	call Atraso_Ctrl
 	call GetMseconds
-	sub eax, Tempo
-	.IF eax > 500
+	sub eax, Tempo	
+	.IF eax > Atraso
 	   call Move_Zep
 	   call GetMseconds
 	   mov Tempo, eax
@@ -341,10 +561,9 @@ Setas:
 
     cmp dx,VK_ESCAPE
  jne Setas   		
-
-
-	 
-
+ Sair:
 	exit
 main ENDP
 END main
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
